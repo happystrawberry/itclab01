d7d469bc739137e4ae11499128eabcc7a7ae83a4
Steven Wu <stevenwu@192-168-1-142.tpgi.com.au>
2021-02-14 19:42:29 +1100

    cleaned up logs in react app

--- src/App copy.js
+++ src/App copy.js
@@ -5,13 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h1>This is CTI Amp-React Lab 0001 </h1>
-        </div>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
